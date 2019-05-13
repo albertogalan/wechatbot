d4d0cef99cab45df45532d490be0fbc3b61198d9
Alberto Galan <info@albertogalan.com>
2019-05-13 14:50:20 +0800

    adding chatterbot

---
+++ chatterbot/proy/train.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+chatbot = ChatBot('Ron Obvious')
+
+# Create a new trainer for the chatbot
+trainer = ChatterBotCorpusTrainer(chatbot)
+
+# Train the chatbot based on the english corpus
+trainer.train("chatterbot.corpus.english")
+
+# Get a response to an input statement
+resp=chatbot.get_response("Do you like films?")
+
+print(resp)
--- examples/starter-bot.js
+++ examples/starter-bot.js
@@ -4,7 +4,9 @@
  */
 var sleep = require('sleep')
 const { Wechaty,Friendship } = require('wechaty')
-const WAIT_REPLY=3
+const WAIT_REPLY=1
+const { FileBox }  = require('file-box')
+const path  = require('path')
 
 function onScan (qrcode, status) {
   require('qrcode-terminal').generate(qrcode, { small: true })  // show qrcode on console
@@ -40,28 +42,59 @@ if(msg.text().match(regex)!==null){
      await sleep.sleep(WAIT_REPLY) 
     if(! msg.room()){
         // reply message if private
-        msg.from().say(reply)
-        }
+        reply.map(n =>sendmsg(msg.from(),n)) 
+                }
  }
 }
+ 
+
+async function sendmsg (receiver,msg){
+    switch (Object.keys(msg)[0]) {
+        case 'text':
+            receiver.say(Object.values(msg)[0])
+            break;
+        case 'file':
+            if (Object.values(msg)[0].match(/http/) !== null)
+
+            {
+                const fileBox = FileBox.fromUrl(Object.values(msg)[0])
+                await receiver.say(fileBox)
+            }
+            else
+            {
+                const fileBox = FileBox.fromFile(Object.values(msg)[0])
+                console.log(fileBox)
+                await receiver.say(fileBox)
+            }
+            
+            break;
+        default:
+    }
+}
+ 
 async function if_keyword_reply_room(msg,regex,reply){
 
 if(msg.text().match(regex)!==null){
      await sleep.sleep(WAIT_REPLY) 
-     const sender = msg.from()
-    if(msg.room()){
-        //reply message if room
-        msg.room().say(reply)
+    if( msg.room()){
+        // reply message if room
+        reply.map(n =>sendmsg(msg.room(),n)) 
+        }
  }
 }
-}
-
 async function onMessage (msg) {
     console.log(msg.toString())
-    await if_keyword_reply_private(msg,/^hello$/,'hi how are you?')
-    await if_keyword_reply_room(msg,/^hi$/,'hello how are you?')
+    saludation=/^(hello2|hii)$/i
+    reply=[
+        {'text':'Hi how are you?'},
+        {'text':'Long time no see you'},
+        {'file':'/data/src/bots/wechaty/tests/test.md'}
+    ]
+    await if_keyword_reply_private(msg,saludation,reply)
+    saludation=/^(你好|您好)$/i
+    await if_keyword_reply_private(msg,saludation,'你好 ')
     if (msg.type() === bot.Message.Type.Attachment){
-        console.log('message is ::' + msg.name())
+        // console.log('message is ::' + msg.name())
         console.log('this message is an Attachment')
         var fs = require('fs');
         fs.writeFile("file.pdf", msg.text(), function(err) {
@@ -91,6 +124,27 @@ bot.on('logout',  onLogout)
 bot.on('message', onMessage)
 bot.on('friendship',onFriendship)
 
-bot.start()
-.then(() => console.log('Starter Bot Started.'))
-.catch(e => console.error(e))
+
+async function init_bot(){
+console.log('Starter Bot Started.')
+
+
+}
+async function main(){
+
+await bot.start()
+  // wait the bot for logging in
+  while (!bot.logonoff()) {
+    await new Promise(r => setTimeout(r, 1200))
+  }
+ var contact= await  bot.Contact.find({ alias:'alberto2'})
+ await contact.say('Hey')
+ const fileBox = FileBox.fromFile('/tmp/text.txt') 
+ await contact.say(fileBox)
+}
+
+main()
+    .then(()=> init_bot())
+.catch(e => {
+  console.error(e)
+})
